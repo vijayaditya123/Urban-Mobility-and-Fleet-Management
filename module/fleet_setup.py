b5556c3a013c42b1092883e836beb94dfaca2a86
Aditya vijay <vijayaditya234789@gmail.com>
2026-01-03 12:03:01 +0530

    UC1: basic fleet setup

---
+++ module/fleet_setup.py
@@ -0,0 +1,5 @@
+class Vehicle:
+    def __init__(self, vehicle_id, model, battery_percentage):
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.battery_percentage = battery_percentage
