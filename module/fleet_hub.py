9927c2daacc32329071e8358df5d26e722bebdbe
Aditya vijay <vijayaditya234789@gmail.com>
2026-01-03 13:45:25 +0530

    UC8: search vehicles by hub and battery status

--- module/fleet_hub.py
+++ module/fleet_hub.py
@@ -1,6 +1,6 @@
 class FleetHub:
     def __init__(self):
-        self.hubs = {}  # hub_name -> list of vehicles
+        self.hubs = {}  
 
     def add_hub(self, hub_name):
         if hub_name not in self.hubs:
@@ -14,3 +14,19 @@ class FleetHub:
             raise ValueError("Duplicate vehicle ID in this hub")
 
         self.hubs[hub_name].append(vehicle)
+
+   
+
+    def search_by_hub(self, hub_name):
+        if hub_name not in self.hubs:
+            return []
+        return self.hubs[hub_name]
+
+    def search_by_battery_above(self, percentage):
+        return list(
+            filter(
+                lambda v: v.get_battery_percentage() > percentage,
+                [v for vehicles in self.hubs.values() for v in vehicles]
+            )
+        )
+
