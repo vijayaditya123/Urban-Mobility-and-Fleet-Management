6fae42e0349e6ff4e6a2d5a1190060b17e178ee7
Aditya vijay <vijayaditya234789@gmail.com>
2026-01-03 13:38:51 +0530

    UC7: prevent duplicate vehicle IDs in fleet hub

--- module/fleet_hub.py
+++ module/fleet_hub.py
@@ -1,23 +1,16 @@
 class FleetHub:
     def __init__(self):
-        self.hubs = {}
+        self.hubs = {}  # hub_name -> list of vehicles
 
     def add_hub(self, hub_name):
         if hub_name not in self.hubs:
             self.hubs[hub_name] = []
-            print(f"Hub '{hub_name}' added successfully.")
-        else:
-            print(f"Hub '{hub_name}' already exists.")
 
-    def add_vehicle_to_hub(self, hub_name, vehicle):
-        if hub_name in self.hubs:
-            self.hubs[hub_name].append(vehicle)
-            print(f"Vehicle {vehicle.vehicle_id} added to hub '{hub_name}'.")
-        else:
-            print(f"Hub '{hub_name}' does not exist.")
+    def add_vehicle(self, hub_name, vehicle):
+        if hub_name not in self.hubs:
+            raise ValueError("Hub does not exist")
 
-    def display_hubs(self):
-        for hub, vehicles in self.hubs.items():
-            print(f"\nHub: {hub}")
-            for v in vehicles:
-                print(f"  - Vehicle ID: {v.vehicle_id}, Model: {v.model}")
+        if vehicle in self.hubs[hub_name]:
+            raise ValueError("Duplicate vehicle ID in this hub")
+
+        self.hubs[hub_name].append(vehicle)
--- module/fleet_setup.py
+++ module/fleet_setup.py
@@ -1,20 +1,22 @@
-from abc import ABC, abstractmethod
-
-
-class Vehicle(ABC):
+class Vehicle:
     def __init__(self, vehicle_id, model, battery_percentage, maintenance_status, rental_price):
         self.vehicle_id = vehicle_id
         self.model = model
         self.set_battery_percentage(battery_percentage)
-        self.set_maintenance_status(maintenance_status)
+        self.__maintenance_status = maintenance_status
         self.set_rental_price(rental_price)
 
+    def __eq__(self, other):
+        if isinstance(other, Vehicle):
+            return self.vehicle_id == other.vehicle_id
+        return False
 
-    @abstractmethod
-    def calculate_trip_cost(self, distance):
-        pass
+    def get_maintenance_status(self):
+        return self.__maintenance_status
 
-  
+    def set_maintenance_status(self, maintenance_status):
+        self.__maintenance_status = maintenance_status
+
     def get_battery_percentage(self):
         return self.__battery_percentage
 
@@ -24,14 +26,6 @@ class Vehicle(ABC):
         else:
             raise ValueError("Battery percentage must be between 0 and 100")
 
-   
-    def get_maintenance_status(self):
-        return self.__maintenance_status
-
-    def set_maintenance_status(self, maintenance_status):
-        self.__maintenance_status = maintenance_status
-
-  
     def get_rental_price(self):
         return self.__rental_price
 
@@ -40,3 +34,4 @@ class Vehicle(ABC):
             self.__rental_price = rental_price
         else:
             raise ValueError("Rental price must be positive")
+
