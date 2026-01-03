fe46136b7e8652d85c32c5ee9688eacfd2b1ef93
Aditya vijay <vijayaditya234789@gmail.com>
2026-01-03 12:08:27 +0530

    UC2: encapsulation and security

--- module/fleet_setup.py
+++ module/fleet_setup.py
@@ -1,5 +1,35 @@
 class Vehicle:
-    def __init__(self, vehicle_id, model, battery_percentage):
+    def __init__(self, vehicle_id, model, battery_percentage, maintenance_status, rental_price):
         self.vehicle_id = vehicle_id
         self.model = model
-        self.battery_percentage = battery_percentage
+        self.set_battery_percentage(battery_percentage)
+        self.set_maintenance_status(maintenance_status)
+        self.set_rental_price(rental_price)
+
+
+    def get_battery_percentage(self):
+        return self.__battery_percentage
+
+    def set_battery_percentage(self, battery_percentage):
+        if 0 <= battery_percentage <= 100:
+            self.__battery_percentage = battery_percentage
+        else:
+            raise ValueError("Battery percentage must be between 0 and 100")
+
+
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+
+    def set_maintenance_status(self, maintenance_status):
+        self.__maintenance_status = maintenance_status
+
+  
+    def get_rental_price(self):
+        return self.__rental_price
+
+    def set_rental_price(self, rental_price):
+        if rental_price > 0:
+            self.__rental_price = rental_price
+        else:
+            raise ValueError("Rental price must be positive")
+
